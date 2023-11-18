e14064c352ecdcf0a117c9f528806862284d4fa3
Phillipp Glanz <p.glanz@madfix.me>
2023-11-18 17:14:33 +0100

    Add project template content

---
+++ src/test/kotlin/net/theevilreaper/template/TemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.theevilreaper.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
