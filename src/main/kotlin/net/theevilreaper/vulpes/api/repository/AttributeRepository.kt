3f52c93884fc0990e0c2f684961335453c74bc76
theEvilReaper <steffenwx@gmail.com>
2024-01-03 22:15:04 +0100

    Add documentation

--- src/main/kotlin/net/theevilreaper/vulpes/api/repository/AttributeRepository.kt
+++ src/main/kotlin/net/theevilreaper/vulpes/api/repository/AttributeRepository.kt
@@ -3,4 +3,10 @@ package net.theevilreaper.vulpes.api.repository
 import net.theevilreaper.vulpes.api.model.AttributeModel
 import org.springframework.data.mongodb.repository.MongoRepository
 
+/**
+ * The repository is used to store different object instances from the [AttributeModel].
+ * It extends the [MongoRepository] to provide the basic CRUD operations.
+ * @see MongoRepository
+ * @author theEvilReaper
+ */
 interface AttributeRepository : MongoRepository<AttributeModel, String>
