886cd8d41133735148dffd7c6ca33beaf0d6da35
theEvilReaper <steffenwx@gmail.com>
2024-01-26 21:09:06 +0100

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,24 @@
 rootProject.name = "vulpes-spring-api"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("kotlin", "1.9.22")
+
+            library("spring.starter", "org.springframework.boot", "spring-boot-starter").withoutVersion()
+            library(
+                "spring.starter.mongodb",
+                "org.springframework.boot",
+                "spring-boot-starter-data-mongodb"
+            ).withoutVersion()
+
+            library("spring.starter.test", "org.springframework.boot", "spring-boot-starter-test").withoutVersion()
+            library("kotlin.reflect", "org.jetbrains.kotlin", "kotlin-reflect").withoutVersion()
+
+            plugin("spring", "org.springframework.boot").version("3.2.1")
+            plugin("spring.dependency", "io.spring.dependency-management").version("1.1.4")
+            plugin("kotlin.jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
+            plugin("kotlin.spring", "org.jetbrains.kotlin.plugin.spring").versionRef("kotlin")
+        }
+    }
+}
