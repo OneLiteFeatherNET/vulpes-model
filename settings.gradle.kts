f496f60f069e8e07e9b86baf7e7b6457fb19391a
theEvilReaper <steffenwx@gmail.com>
2025-02-25 21:19:38 +0100

    Bump micronaut

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "vulpes-spring-api"
 
 plugins {
-    id("io.micronaut.platform.catalog") version "4.4.4"
+    id("io.micronaut.platform.catalog") version "4.4.5"
 }
 
 dependencyResolutionManagement {
