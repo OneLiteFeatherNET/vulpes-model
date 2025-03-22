747df5886df49b9903db0e1052b4902aede83f83
theEvilReaper <steffenwx@gmail.com>
2025-03-22 14:33:43 +0100

    Remove publishdata usage

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,3 @@ rootProject.name = "vulpes-model"
 plugins {
     id("io.micronaut.platform.catalog") version "4.4.5"
 }
-
-dependencyResolutionManagement {
-    pluginManagement {
-        repositories {
-            gradlePluginPortal()
-            maven("https://eldonexus.de/repository/maven-public/")
-        }
-    }
-}
