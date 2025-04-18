3ae990188a872af83f95c4e5958f7b74f81cbd74
Joltras <MinnyMouseXD@gmail.com>
2025-04-18 14:46:28 +0200

    Update model class usage

--- src/main/java/net/theevilreaper/vulpes/api/repository/AttributeRepository.java
+++ src/main/java/net/theevilreaper/vulpes/api/repository/AttributeRepository.java
@@ -2,10 +2,10 @@ package net.theevilreaper.vulpes.api.repository;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
-import net.theevilreaper.vulpes.api.model.AttributeModel;
+import net.theevilreaper.vulpes.api.model.AttributeEntity;
 
 import java.util.UUID;
 
 @Repository
-public interface AttributeRepository extends PageableRepository<AttributeModel, UUID> {
+public interface AttributeRepository extends PageableRepository<AttributeEntity, UUID> {
 }
