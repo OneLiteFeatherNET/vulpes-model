b514beed738ad122cdefea196a842ab73921df31
Joltras <72395471+Joltras@users.noreply.github.com>
2025-04-18 14:51:33 +0200

    Rename model suffix to entity (#12)
    * Rename classes from model to entity
    
    * Update model class usage

--- src/main/java/net/theevilreaper/vulpes/api/repository/NotificationRepository.java
+++ src/main/java/net/theevilreaper/vulpes/api/repository/NotificationRepository.java
@@ -2,10 +2,10 @@ package net.theevilreaper.vulpes.api.repository;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
-import net.theevilreaper.vulpes.api.model.NotificationModel;
+import net.theevilreaper.vulpes.api.model.NotificationEntity;
 
 import java.util.UUID;
 
 @Repository
-public interface NotificationRepository extends PageableRepository<NotificationModel, UUID> {
+public interface NotificationRepository extends PageableRepository<NotificationEntity, UUID> {
 }
