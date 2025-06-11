3562e053e0beba7d9ee8bbe543d5462ea2abc676
Steffen Wonning <steffenwx@gmail.com>
2025-06-11 22:07:50 +0200

    Update entities which are related to sounds (#24)
    * Update SoundData definition to match the latest release of the game #23
    
    * Update SoundEntity to match with the latest release of the game #23
    
    * Rename sound files for better understanding and update documentation #23
    
    * Update model import and add class header documentation #23
    
    * Fix documentation #23

--- src/main/java/net/onelitefeather/vulpes/api/repository/SoundRepository.java
+++ src/main/java/net/onelitefeather/vulpes/api/repository/SoundRepository.java
@@ -2,10 +2,18 @@ package net.onelitefeather.vulpes.api.repository;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
-import net.onelitefeather.vulpes.api.model.sound.SoundEntity;
+import net.onelitefeather.vulpes.api.model.sound.SoundEventEntity;
 
 import java.util.UUID;
 
+/**
+ * The {@link SoundRepository} interface defines a crud repository with the pageable functionality to manage
+ * different {@link SoundEventEntity} entities in a database.
+ *
+ * @author theEvilReaper
+ * @version 1.5.0
+ * @since 0.1.0
+ */
 @Repository
-public interface SoundRepository extends PageableRepository<SoundEntity, UUID> {
+public interface SoundRepository extends PageableRepository<SoundEventEntity, UUID> {
 }
