ff781feeabe7da3986201a74da62060334f45745
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-14 21:12:18 +0200

    Add method to get all chars from a model

--- src/main/java/net/onelitefeather/vulpes/api/repository/FontRepository.java
+++ src/main/java/net/onelitefeather/vulpes/api/repository/FontRepository.java
@@ -1,11 +1,22 @@
 package net.onelitefeather.vulpes.api.repository;
 
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
 import net.onelitefeather.vulpes.api.model.FontEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FontRepository extends PageableRepository<FontEntity, UUID> {
+
+    /**
+     * Retrieves the characters associated with a font by its ID.
+     *
+     * @param id the unique identifier of the font
+     * @return a list of characters associated with the font
+     */
+    @Query("SELECT c FROM fonts f JOIN f.chars c WHERE f.id = :id")
+    List<String> findCharsByFontId(UUID id);
 }
