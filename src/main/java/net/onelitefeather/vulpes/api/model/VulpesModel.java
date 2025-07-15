eaae7b3c9c3441bfd0ea2fbe1876c07c6c819dc3
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-15 11:25:57 +0200

    docs(model): Add documentation to the marker interface

--- src/main/java/net/onelitefeather/vulpes/api/model/VulpesModel.java
+++ src/main/java/net/onelitefeather/vulpes/api/model/VulpesModel.java
@@ -1,4 +1,15 @@
 package net.onelitefeather.vulpes.api.model;
 
+/**
+ * Marker interface for classes that are part of the Vulpes model definition.
+ * <p>
+ * This interface is used to restrict certain functionality or behavior to classes
+ * that explicitly implement it, serving as a means of semantic grouping within the model layer.
+ * </p>
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public interface VulpesModel {
 }
