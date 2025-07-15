3515dde062de50c62b1fe2c9e217026128154246
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-15 11:25:44 +0200

    docs(generator): Add documentation to the uuid generator

--- src/main/java/net/onelitefeather/vulpes/api/generator/UuidV7Generator.java
+++ src/main/java/net/onelitefeather/vulpes/api/generator/UuidV7Generator.java
@@ -9,13 +9,34 @@ import java.util.EnumSet;
 
 import static org.hibernate.generator.EventTypeSets.INSERT_ONLY;
 
+/**
+ * The {@link UuidV7Generator} class implements a generator that creates time-ordered UUIDs (version 7) for new entities.
+ *
+ * @author TheMeinerLP
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public final class UuidV7Generator implements BeforeExecutionGenerator {
 
+    /**
+     * Generates a time-ordered UUID (version 7) for the given entity.
+     *
+     * @param session      the session in which the entity is being created
+     * @param owner        the owner of the entity
+     * @param currentValue the current value of the UUID field (not used)
+     * @param eventType    the type of event triggering this generation (should be INSERT)
+     * @return a new time-ordered UUID
+     */
     @Override
     public Object generate(SharedSessionContractImplementor session, Object owner, Object currentValue, EventType eventType) {
         return UuidCreator.getTimeOrderedEpoch();
     }
 
+    /**
+     * Returns the set of event types for which this generator is applicable.
+     *
+     * @return an EnumSet containing the INSERT_ONLY event type
+     */
     @Override
     public EnumSet<EventType> getEventTypes() {
         return INSERT_ONLY;
--- src/main/java/net/onelitefeather/vulpes/api/generator/VulpesGenerator.java
+++ src/main/java/net/onelitefeather/vulpes/api/generator/VulpesGenerator.java
@@ -9,8 +9,22 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Annotation to mark a field or method for UUIDv7-based ID generation within the Vulpes model.
+ * <p>
+ * This annotation is a specialization that applies {@link UuidV7Generator} as the ID generation strategy.
+ * It can be applied to fields or methods to instruct the persistence framework to generate a UUIDv7
+ * when persisting the entity.
+ * </p>
+ *
+ * @author TheMeinerLP
+ * @version 1.0.0
+ * @see UuidV7Generator
+ * @see IdGeneratorType
+ * @since 0.1.0
+ */
 @IdGeneratorType(UuidV7Generator.class)
 @Retention(RUNTIME)
-@Target({METHOD,FIELD})
+@Target({METHOD, FIELD})
 public @interface VulpesGenerator {
 }
