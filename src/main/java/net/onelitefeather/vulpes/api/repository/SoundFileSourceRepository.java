0b7e6297aedac656c642d53c8c9f6b48d7b10ae9
Phillipp Glanz <p.glanz@madfix.me>
2025-08-30 20:32:56 +0200

    fix(SoundFileSourceRepository): remove unused import for NotificationEntity

--- src/main/java/net/onelitefeather/vulpes/api/repository/SoundFileSourceRepository.java
+++ src/main/java/net/onelitefeather/vulpes/api/repository/SoundFileSourceRepository.java
@@ -2,7 +2,6 @@ package net.onelitefeather.vulpes.api.repository;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.PageableRepository;
-import net.onelitefeather.vulpes.api.model.NotificationEntity;
 import net.onelitefeather.vulpes.api.model.sound.SoundFileSource;
 
 import java.util.UUID;
