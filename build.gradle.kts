6b68a21c0a401a07f95693c530bca09025028660
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-11-07 14:20:51 +0100

    chore: update Java version to 25 and Gradle to 9.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
     withSourcesJar()
     withJavadocJar()
@@ -28,7 +28,7 @@ dependencies {
 tasks {
     compileJava {
         options.encoding = "UTF-8"
-        options.release.set(21)
+        options.release.set(25)
     }
 
     test {
