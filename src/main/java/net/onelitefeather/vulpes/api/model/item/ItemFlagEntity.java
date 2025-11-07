78ff62b66d22b317f0cde22962677562cf2caa46
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-07 14:59:27 +0100

    chore(entity): convert records back to normal classes

--- src/main/java/net/onelitefeather/vulpes/api/model/item/ItemFlagEntity.java
+++ src/main/java/net/onelitefeather/vulpes/api/model/item/ItemFlagEntity.java
@@ -15,9 +15,6 @@ import java.util.UUID;
 /**
  * The {@link ItemFlagEntity} represents a flag or attribute that can be associated with an item in the system.
  *
- * @author theEvilReaper
- * @version 1.1.0
- * @since 1.6.0
  */
 @Entity(name = "item_flags")
 public final class ItemFlagEntity {
@@ -25,82 +22,49 @@ public final class ItemFlagEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     @VulpesGenerator
     private UUID id;
-    private String flag;
+    private String text;
     @ManyToOne
     @JoinColumn(name = "item_id")
     private ItemEntity item;
 
     /**
-     * Default constructor for JPA and Micronaut Data.
-     */
-    public ItemFlagEntity() {
-        // No-argument constructor for JPA
-    }
-
-    /**
-     * Constructs a new {@link ItemFlagEntity} with the specified parameters.
-     *
      * @param id   the unique identifier of the item flag
-     * @param flag the descriptive text of the item flag
+     * @param text the descriptive text of the item flag
      */
     public ItemFlagEntity(
             UUID id,
-            String flag
+            String text,
+            ItemEntity item
     ) {
         this.id = id;
-        this.flag = flag;
+        this.text = text;
+        this.item = item;
     }
 
-    /**
-     * Sets the unique identifier of the item flag.
-     *
-     * @param id the unique identifier to set
-     */
+    public ItemFlagEntity() {
+
+    }
+
     public void setId(UUID id) {
         this.id = id;
     }
 
-    /**
-     * Gets the unique identifier of the item flag.
-     *
-     * @return the unique identifier
-     */
     public UUID getId() {
         return id;
     }
 
-    /**
-     * Sets the flag associated with the item.
-     *
-     * @param flag the item flag
-     */
-    public void setFlag(String flag) {
-        this.flag = flag;
+    public void setText(String text) {
+        this.text = text;
     }
 
-    /**
-     * Gets the flag associated with the item.
-     *
-     * @return the item flag
-     */
-    public String getFlag() {
-        return flag;
+    public String getText() {
+        return text;
     }
 
-    /**
-     * Sets the item associated with this flag.
-     *
-     * @param item the item to associate
-     */
     public void setItem(ItemEntity item) {
         this.item = item;
     }
 
-    /**
-     * Gets the item associated with this flag.
-     *
-     * @return the associated item
-     */
     public ItemEntity getItem() {
         return item;
     }
@@ -111,20 +75,22 @@ public final class ItemFlagEntity {
         if (obj == null || obj.getClass() != this.getClass()) return false;
         var that = (ItemFlagEntity) obj;
         return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.flag, that.flag) &&
+                Objects.equals(this.text, that.text) &&
                 Objects.equals(this.item, that.item);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, flag, item);
+        return Objects.hash(id, text, item);
     }
 
     @Override
     public String toString() {
         return "ItemFlagEntity[" +
                 "id=" + id + ", " +
-                "text=" + flag + ", " +
+                "text=" + text + ", " +
                 "item=" + item + ']';
     }
+
+
 }
